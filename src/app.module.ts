4cc937f9ce7034d6fc71cdf2d03dd0c184603b5d
Luccas Brandao <srluccasonline@gmail.com>
2021-10-13 15:55:25 -0300

    :sparkles: Add TypeORM + MySQL config

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import * as dotenv from 'dotenv'
+import { TypeOrmModule } from '@nestjs/typeorm';
 dotenv.config()
 
 @Module({
   imports: [AuthModule,
     MongooseModule.forRoot(process.env.MONGO),
+    TypeOrmModule.forRoot()
   ],
   controllers: [AppController],
   providers: [AppService],
