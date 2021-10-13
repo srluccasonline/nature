fb684a0cdd5b98121a8e8d1328132d887ea336ad
Luccas Brandao <srluccasonline@gmail.com>
2021-10-12 22:05:57 -0300

    :sparkles: create dto auth

--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -1,1 +1,11 @@
-export class CreateAuthDto {}
+export class CreateAuthDto {
+    email: string;
+    fullName: string;
+    password: string;
+    phoneNumber: number;
+    city: string;
+    address: string;
+    addressNumber: number;
+    district: string;
+    complement?: string;
+}
